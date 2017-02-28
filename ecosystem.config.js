12b590b4722128ca35142492d08f90188aa28fcd
gamaliel <fabiogamaliel@gmail.com>
2017-02-28 13:40:57 -0400

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	apps: [{
+		name: 'app-node',
+		script: './index.js'
+	}],
+	deploy: {
+		production: {
+			user: 'ubuntu',
+			host: 'http://ec2-52-43-101-31.us-west-2.compute.amazonaws.com',
+			key: '~/.ssh/contabilidadjg.pem',
+			ref: 'origin/master',
+			repo: 'git@github.com:gamalielsaracho/app-node.git',
+			path: '/home/ubuntu/app-node',
+			'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+		}
+	}
+}
