a8edf879cefea1445a340382f6bd9ee31a34b30b
gamaliel <fabiogamaliel@gmail.com>
2017-02-28 13:55:04 -0400

    new message

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hola mundo. cool.!')
+  res.send('Hellooo. cool.!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
